824d83eafccbfed9fbe0ee07529fb14b2cc12c5f
’Pieter <azize.ergun@student.hogent.be>
2020-10-29 20:12:08 +0100

    Initial Commit

---
+++ IOS_SELFHELP/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  IOS_SELFHELP
+//
+//  Created by Azize Ergun on 29/10/2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
