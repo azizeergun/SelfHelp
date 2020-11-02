99f75b16c19d427f63b0bfdd50551fb5cf500aa0
Azize <azize.ergun@student.hogent.be>
2020-11-02 16:41:37 +0100

    nieuwe scherm aangemaakt maar de collectionview kan je nog niet zien

---
+++ IOS_SELFHELP/MyCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  MyCollectionViewCell.swift
+//  IOS_SELFHELP
+//
+//  Created by Azize Ergun on 02/11/2020.
+//
+
+import UIKit
+
+class MyCollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var myLabel: UILabel!
+}
--- IOS_SELFHELP/ThemeButtonScreen.swift
+++ IOS_SELFHELP/ThemeButtonScreen.swift
@@ -16,7 +16,7 @@ class ThemeButtonScreen: UIViewController {
     }
     
     @IBAction func MentalHealthButton(_ sender: UIButton) {
-        Onderverdeling_MentalHealth()
+     
     }
     
     @IBAction func AssaultButton(_ sender: UIButton) {
